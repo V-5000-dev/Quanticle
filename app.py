20e4684aa8e4cb0d5d1978a7cb9a0a8e2b0723f3
V-5000-dev <victor101001000@gmail.com>
2026-04-09 22:58:02 -0400

    WORKS WORKS WORKS WORKS WORKS WORKS WORKS WORKS

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print(__file__)
 
 db.init_app(app)
 login_manager.init_app(app)
--- models/user.py
+++ models/user.py
@@ -1,6 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
- 
-db = SQLAlchemy()
-login_manager = LoginManager()
- 
+from flask_login import UserMixin
+from extensions import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
